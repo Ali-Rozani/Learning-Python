bcecb4628741a9cce333ea6324ce12b810b7a67e
Ali-Rozani <ah.rozani@yahoo.com>
2025-02-23 17:41:24 +0500

    Create Math_Moudule.py

---
+++ Math_Moudule.py
@@ -0,0 +1,26 @@
+import math
+
+# Square root
+Square_Root: int = math.sqrt(16)
+print(Square_Root)
+# Power
+Power: int = math.pow(17, 10)
+print(Power)
+# Round
+Round: float = round(3.9)
+print(Round)
+# Ceil
+Ceil: float = math.ceil(92.5)
+print(Ceil)
+# Floor
+Floor: int = math.floor(15)
+print(Floor)
+# Factorial
+Factorial: int = math.factorial(5)
+print(Factorial)
+# GCD
+GCD: int = math.gcd(12, 15)
+print(GCD)
+# Log
+Log: float = math.log(10)
+print(Log)
